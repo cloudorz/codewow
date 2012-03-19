cc55b764e9ba5b83cbed7844608333955ad171f3
cloud <cloudcry@gmail.com>
2012-03-19 12:42:40 +0800

    add POST

--- codewow/views/deploy.py
+++ codewow/views/deploy.py
@@ -6,7 +6,7 @@ from flask import g, Module
 
 deploy = Module(__name__)
 
-@deploy.route("/git_update")
+@deploy.route("/git_update", methods=('POST',))
 def git_update():
     subprocess.call("cd /data/web/codewow/ && git pull &&\
             cp -a codewow/static /data/web/")
