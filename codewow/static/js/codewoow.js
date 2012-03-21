90ae42175fc2bd0209ef8c2a2e4e65a2e02fd954
fangqi <fangqi.me@gmail.com>
2012-03-21 17:33:51 +0800

    a

--- codewow/static/js/codewoow.js
+++ codewow/static/js/codewoow.js
@@ -167,7 +167,7 @@ $(document).ready(function(){
                    resultArr.push(i);
                 }
             }
-           searchResult.children().remove();
+           $('#dorpDown ul.searchResult li').remove();
            alert(searchResult.children().length);
             for(var j = 0,b=resultArr.length; j<b;j++){
                searchResult.append('<li>'+ arrOption[resultArr[j]]+ '</li>'); 
